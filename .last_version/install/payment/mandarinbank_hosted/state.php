0fc03f79f770b93fc7dee3289ae73b533ea3dbb2
Igor Vuchastyi <vuchastyi.igor@gmail.com>
2017-08-08 15:46:37 +0300

    2017/0808 15:46

---
+++ .last_version/install/payment/mandarinbank_hosted/state.php
@@ -0,0 +1,17 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+use \Bitrix\Main\Localization\Loc;
+Loc::loadMessages(__FILE__);
+
+if(isset($_GET['status'])){
+	if($_GET['status'] == 'success'){
+		echo "��� ����� ������� ������� � ������� ��������� ������� <a href=\"http://mandarinbank.com\" target=\"_blank\">�������� ����</a>\r\n";
+
+	} else {
+		echo "��������� ������ ������ ������ � <b>" . $_GET['orderId'] . "</b> � ������� ��������� ������� <a href=\"http://mandarinbank.com\" target=\"_blank\">�������� ����</a>\r\n! ���������� ��� ���.";
+	}
+}
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
